fefa2e0c90c14f452283579472d4ea346597c4d5
Milan van Niekekr <milan@vanniekerks.com>
2021-02-10 07:41:12 +0200

    modulate

--- app.js
+++ app.js
@@ -36,14 +36,14 @@ app.use(function (req, res, next) {
   res.status(404).render("404");
 });
 
-var server = https.createServer(options, app);
+// var server = https.createServer(options, app);
 
-server.listen(port, () => {
-  console.log("Listening on port: " + port);
-});
+// server.listen(port, () => {
+//   console.log("Listening on port: " + port);
+// });
 
-// app.listen(port, function(){
-//   console.log('Listening on ' + port)
-// })
+app.listen(port, function(){
+  console.log('Listening on ' + port)
+})
 
 // NODE_TLS_REJECT_UNAUTHORIZED=0
